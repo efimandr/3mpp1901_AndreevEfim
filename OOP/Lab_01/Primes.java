deeb93c66b9e3a41332676013849f33274ad9731
efimandr <56430660+efimandr@users.noreply.github.com>
2020-01-09 03:55:47 +0300

    Update Primes.java
    исправил коммент для случая n == i

--- OOP/Lab_01/Primes.java
+++ OOP/Lab_01/Primes.java
@@ -6,8 +6,8 @@ public class Primes {
     }
     public static boolean isPrime(int n) {      //Создаем метод проверки числа n на принадлежность к простым числам
         boolean prime = false;
-        if (n == 2) prime = true;  // по условию задания (условия) к циклу
-        for (int i = 2; i < n; i++) {
+        if (n == 2) prime = true;  // n = 2 "исключение", в цикле ниже i == n не пройдет, отдельная проверка засорит цикл
+        for (int i = 2; i < n; i++) { 
             if ((n % i) == 0) {
                 prime = false;
                 break;
